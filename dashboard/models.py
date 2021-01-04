06376d2f9189b29199ea3752982458f708313501
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-04 23:52:31 +0530

    Add task model

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,6 +4,20 @@ from django.db import models
 from core.models import BaseModel
 
 
+class Task(BaseModel):
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='tasks_created')
+    assignee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='tasks_assigned')
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(BaseModel):
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
 
