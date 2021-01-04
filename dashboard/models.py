f3ef57ac007da6730a4f7afdf439d230b874e63a
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-04 23:43:20 +0530

    Add project model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Project(BaseModel):
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    duration_in_months = models.PositiveSmallIntegerField(null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
