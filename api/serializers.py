2aa67cbf41d0087182cb9bd7fa11e7b78d91215a
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-05 09:27:28 +0530

    Add user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,17 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from dashboard.models import Project
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'pk')
+
 class ProjectSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(many=False, read_only=True)
+
     class Meta:
         model = Project
         fields = '__all__'
