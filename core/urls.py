6d3139eebe0711cadf5c90b3ce9b30366e9b536b
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-05 07:25:39 +0530

    Fix url name

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ from core import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('register/', views.RegisterView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
