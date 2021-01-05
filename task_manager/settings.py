ebeaf73845b656c00e306ab5ccde848d60b6e291
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-05 08:47:02 +0530

    Register rest framework

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     # Project apps
     'api.apps.ApiConfig',
     'core.apps.CoreConfig',
