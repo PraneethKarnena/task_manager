4bf79a9df0d5429c4dbda94d35714b8f9d65811d
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-05 07:47:10 +0530

    Add logout route

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
