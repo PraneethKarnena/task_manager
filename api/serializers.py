c9de169a486fac89c9217c43a331cd4a643c2875
Praneeth Karnena <praneeth.codes@gmail.com>
2021-01-05 09:30:17 +0530

    Add task serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,24 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from dashboard.models import Project
+from dashboard.models import Project, Task, SubTask
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('first_name', 'last_name', 'pk')
-        
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(many=False, read_only=True)
+    tasks = TaskSerializer(many=True)
 
     class Meta:
         model = Project
